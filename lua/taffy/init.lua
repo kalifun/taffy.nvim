073d281abdd1529f39dd111ff3005b12df51937f
kalifun <kalifun@163.com>
2023-02-07 08:20:52 +0800

    🚧 update taffy/init.lua

--- lua/taffy/init.lua
+++ lua/taffy/init.lua
@@ -1,8 +1,36 @@
 local o = vim.o
 local g = vim.g
 
+local DEFAULT_CONFIG = {
+	italic_comment = false,
+	transparent_bg = false,
+	show_end_of_buffer = false,
+	lualine_bg_color = nil,
+	colors = require("taffy.palette"),
+	overrides = {},
+}
+
+local local_configs = DEFAULT_CONFIG
+
+local TRANSPARENTS = {
+	"Normal",
+	"SignColumn",
+	"NvimTreeNormal",
+	"NvimTreeVertSplit",
+ }
+
 local function load()
 	o.background = "dark"
 	o.termguicolors = true
-	g.colors_name = "dracula"
+	g.colors_name = "taffy"
 end
+
+return {
+	load = load,
+	configs = function()
+		return local_configs
+	end,
+	colors = function()
+		return local_configs.colors
+	end,
+}
